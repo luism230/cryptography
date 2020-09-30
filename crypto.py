59be74176a3405cd270094ee53369230689541c4
Luis Ramirez Mendoza <luism.ramirez23@gmail.com>
2020-09-29 21:26:48 -0700

    FINISHED

--- crypto.py
+++ crypto.py
@@ -1,51 +1,64 @@
 import string
 import random
 import math
+
+def get_vKey(key, mlength):
+    newkey = ''
+    while len(newkey) < mlength:
+        for i in key:
+            if len(newkey) < mlength:
+                newkey += i
+    return newkey 
+
 # Caesar Cipher
 # Arguments: string, integer
 # Returns: string
 def encrypt_caesar(plaintext, offset):
-    end = ord('z')
-    start = ord('a') - 1
+    end = ord('Z')
+    start = ord('A') - 1
 
-    while offset > end:
-        offset -= end
+    if offset > 26:
+        offset = offset%26
 
     newText = ''
     for i in plaintext:
         value = ord(i)
-        if value + offset > end:
+
+        if value > end or value < start + 1:
+            newText += chr(value)
+        elif value + offset > end:
             new_offset = offset - (end - value)
             newText += chr(start + new_offset)
         else:
-            newText += chr(ord(i) + offset)
+            newText += chr(value + offset)
     return newText
 # Arguments: string, integer
 # Returns: string
 def decrypt_caesar(ciphertext, offset):
-    end = ord('z') + 1
-    start = ord('a')
+    end = ord('Z') + 1
+    start = ord('A')
 
-    while offset > end -1:
-        offset -= end -1
+    if offset > 26:
+        offset = offset%26
 
     original = ''
     for i in ciphertext:
         value = ord(i)
-        if value - offset < start:
+        if value > end - 1 or value < start:
+            original += chr(value)
+        elif value - offset < start:
             new_offset = offset - (value - start)
             original += chr(end - new_offset)
         else:
-            original += chr(ord(i) - 1)
+            original += chr(value - offset)
     return original
 
 # Vigenere Cipher
 # Arguments: string, string
 # Returns: string
 def encrypt_vigenere(plaintext, keyword):
-    alphabet = list(string.ascii_lowercase)
-    end = ord('z')
-    start = ord('a') - 1
+    keyword = get_vKey(keyword, len(plaintext))
+    alphabet = list(string.ascii_uppercase)
     newText = ''
 
     count = 0
@@ -59,7 +72,8 @@ def encrypt_vigenere(plaintext, keyword):
 # Arguments: string, string
 # Returns: string
 def decrypt_vigenere(ciphertext, keyword):
-    alphabet = list(string.ascii_lowercase)
+    keyword = get_vKey(keyword, len(ciphertext))
+    alphabet = list(string.ascii_uppercase)
     count = 0
     newText = ''
 
@@ -106,111 +120,77 @@ def create_public_key(private_key):
 
     return B
 
+def bytes_to_bits(byte):
+    bits = []
+    for i in byte:
+        bit = bin(i)
+        bit = bit.replace('b', '')
+        while len(bit) < 8:
+            bit = '0' + bit
+        bits.append(bit) 
+    return bits
+
+def bits_to_bytes(bit):
+    byte = []
+    for i in bit:
+        byte.append(int(i,2))
+    message = ''
+    for i in byte:
+        message += chr(i)
+    return message
 
 # Arguments: string, tuple (W, Q, R)
 # Returns: list of integers
 def encrypt_mhkc(plaintext, public_key):
-    pass
+    byte = []
+    for i in plaintext:
+        byte.append(ord(i))
+    bits = bytes_to_bits(byte)
+    encrypted = []
+    for i in bits:
+        C = 0
+        for j in range(0,8):
+            C+=(int(i[j])*public_key[j])
+
+        encrypted.append(C)
+    return encrypted
+
+
 
 # Arguments: list of integers, tuple B - a length-n tuple of integers
 # Returns: bytearray or str of plaintext
 def decrypt_mhkc(ciphertext, private_key):
-    pass
+    W = private_key[0]
+    Q = private_key[1]
+    R = private_key[2]
+    S = 1
+    while (R*S)%Q != 1:
+        S+=1
 
-def get_encryption_method():
-    method = input("Enter 'c' for Caeser Cypher\nEnter 'v' for Vignere\nor Enter 'm' for MHK Cryptosystem\n")
-    method = method.lower()
-    while method != 'c' and method != 'v' and method != 'm':
-        print('Incorrect Input. Try again')
-        encryption_method = input("Enter 'c' for Caeser Cypher\nEnter 'v' for Vignere\nor Enter 'm' for MHK Cryptosystem\n")
-    return method
+    cPrime = []
+    for i in ciphertext:
+        cPrime.append((i*S)%Q)
+    
+    
+    decrypted = []
+    for i in cPrime:
+        m = ''
+        for j in reversed(W):
+            if i >= j:
+                m = '1' + m 
+                i = i - j
+            else:
+                m = '0' + m
+        decrypted.append(m)
 
-def get_message():
-    word = input("Enter Input (No non-alphabetical characters)\n")
-     #Prevent user from inputting non-alphabetical character
-    while len(word) < 1:
-        print("Input must be at least one character long")
-        word = input("Enter Input (No non-alphabetical characters)\n")
-
-    wrong = False
-    for i in word:
-        if i not in string.ascii_lowercase:
-            wrong = True
-
-    while wrong:
-        wrong = False
-        print("Incorrect Input. No non-alphabetical characters allowed")
-        word = input("Enter Input (No non-alphabetical characters)\n")
-        for i in word:
-            if i not in string.ascii_lowercase:
-                wrong = True
-    return word
-
-def get_num():
-    number = input("Enter Offset Number\n")
-    while number.isnumeric() == False:
-        print("Incorrect Input. Please only use numbers")
-        number = input("Enter number to offset\n")
-    return int(number)
-
-
-def get_vKey(message):
-    print('Enter Key')
-    key = get_message()
-    newkey = ''
-    while len(newkey) < len(message):
-        for i in key:
-            if len(newkey) < len(message):
-                newkey += i.lower()
-    return newkey
+    return bits_to_bytes(decrypted)
 
 
 def main():
     # Testing code here
-    
-    #  Will only accept 'e' or 'd' as an option
-    answer = input("Enter 'e' to Encrypt\nEnter 'd' to Decrypt\n")
-    while answer.lower() != 'e' and answer != 'd':
-        print('Incorrect Input. Try again')
-        answer = input("Enter 'e' to Encrypt\nEnter 'd' to Decrypt\n")
-    if answer == 'e':
-        
-        # Prevents wrong inputs
-        encryption_method = get_encryption_method()
-        if encryption_method.lower() == 'c':
-            message = get_message()
-            number = get_num()
-
-            print(encrypt_caesar(message, number))
-
-        elif encryption_method.lower() == 'v':
-            message = get_message()
-
-            key = get_vKey(message)
-            print(encrypt_vigenere(message, key))
-        else:
-            pass
-    else:
-
-        # Prevents wrong inputs
-        decryption_method = get_encryption_method()
-        if decryption_method.lower() == 'c':
-            message = get_message()
-            number = get_num()
-            print(decrypt_caesar(message, number))
-        elif decryption_method.lower() == 'v':
-            message = get_message()
-            key = get_vKey(message)
-            print(decrypt_vigenere(message, key))
-        else:
-            pass
-    
-
         
 
 if __name__ == "__main__":
-    generate_private_key()
-
-    
+    main()
 
 
